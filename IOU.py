7b9a7c819dc7fc66b052a1ce0a3527a6f39d0acd
mhadar <mor.hadar@intel.com>
2022-01-20 11:52:51 +0200

    init repo

---
+++ IOU.py
@@ -0,0 +1,36 @@
+# %%
+import numpy as np
+from scipy import ndimage
+import matplotlib.pyplot as plt
+
+##
+a = np.zeros((20,20))
+a[5:15,5:15] = 1
+b = a.copy()
+b = ndimage.rotate(b, 45, reshape=False)
+b[b>0.5] = 1
+b[b<0.5] = 0
+# plt.matshow(a)
+# plt.matshow(b)
+
+a = a.astype('bool')
+b = b.astype('bool')
+def iou(a, b):
+    "intersection over union for 2 classes only."
+    intersection = (a & b).sum()
+    union = a.sum() + b.sum() - intersection
+    return intersection/union
+
+def f1_score(a,b):
+    "aka Dice coefficient"
+    twos_intersection = 2* (a & b).sum()
+    all = a.sum() + b.sum()
+    return twos_intersection/all
+
+print(f' iou= {iou(a,b)}')
+print(f' f1= {f1_score(a,b)}')
+
+#%% #! resources:
+# https://tomkwok.com/posts/iou-vs-f1/
+# https://stats.stackexchange.com/questions/273537/f1-dice-score-vs-iou/276144#276144 
+
---
+++ random_trees.py
@@ -0,0 +1,48 @@
+
+# %% 
+# %matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+
+
+# %%
+from sklearn.datasets import make_blobs
+X, y = make_blobs(n_samples=300, centers=4,
+                  random_state=0, cluster_std=1.0)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='rainbow');
+
+# %%
+
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier().fit(X, y)
+#%%
+def visualize_classifier(model, X, y, ax=None, cmap='rainbow'):
+    ax = ax or plt.gca()
+    
+    # Plot the training points
+    ax.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=cmap,
+               clim=(y.min(), y.max()), zorder=3)
+    ax.axis('tight')
+    ax.axis('off')
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    # fit the estimator
+    model.fit(X, y)
+    xx, yy = np.meshgrid(np.linspace(*xlim, num=200),
+                         np.linspace(*ylim, num=200))
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+
+    # Create a color plot with the results
+    n_classes = len(np.unique(y))
+    contours = ax.contourf(xx, yy, Z, alpha=0.3,
+                           levels=np.arange(n_classes + 1) - 0.5,
+                           cmap=cmap, clim=(y.min(), y.max()),
+                           zorder=1)
+
+    ax.set(xlim=xlim, ylim=ylim)
+# %%
+visualize_classifier(DecisionTreeClassifier(), X, y)
+
+# %%
